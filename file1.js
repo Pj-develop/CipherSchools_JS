7054d69ba1f5bd99fca2f8d0188b8f78be2cfd18
Priyanshu <Priyanshu.k4444@gmail.com>
2023-08-27 11:09:13 +0530

    day2

---
+++ file1.js
@@ -0,0 +1,148 @@
+//basic
+
+console.log("HELLO");
+var pj = "Priaynshu Jha";
+let dj = "Priyanshu";
+//let does not re-define
+console.log(dj);
+//$ can be used
+
+const pie = 3.14;
+console.log(pie);
+
+//string
+
+var name = "Priyanshu";
+console.log(name[3]);
+console.log(name.length);
+
+let myname = "Priyanshu Jha  ";
+console.log(myname.length);
+let newString = myname.trim();
+console.log(newString.length);
+
+// trim()
+// toUpperCase()
+// toLowerCase()
+// slice()
+let FirstName = "Priyanshu";
+//console.log(firstName.length);
+// firstName = firstName.trim(); // "harshit"
+// console.Zog(firstName);
+// console.Zog(firstName.length);
+// stName.toUpperCase();
+// FirstName = firstName = firstName.toLowerCase();
+// console.Zog(firstName);
+// start index
+// end index
+// let newString = firstName.
+console.log(newString);
+// pj
+//data type
+// string;
+// typeof operator
+// data types (primitive data types.
+// string "harhit"
+// number 2, 4, 5.6
+// boo Z eans
+// undefined
+// Biglnt
+// Symbo Z
+let age = 22;
+let FirstaName = "Pj";
+
+// convert number to string.
+age = age + console.log(typeof age);
+// convert string to number.
+let myStr = +"34";
+console.log(typeof myStr);
+
+//or modifier
+// String
+// Number
+
+//format  specifioer
+// template string
+let agee = 22;
+let firstName = "harshit";
+let aboutme = `my name is ${agee} and my name is ${FirstName}`;
+console.log(aboutme);
+
+// booleans & comparison operator
+// booleans
+// true, false
+// let numl
+// let num2
+// console. ZogCnum1<num2);
+// == vs ===
+// == no data type
+// === checkk data type
+
+//cond if else if  else
+
+let mera = 5;
+if (mera == 5) {
+  console.log("Mai 5 hoon");
+} else {
+  console.log("mai 5 nahi hoo");
+} // ** %
+
+//ternary operatpr
+let a = 232,
+  b = 13;
+let drink = a > b ? "coda" : "soda";
+console.log(drink);
+
+// and operator &&
+// or operator  ||
+
+// nested if else
+// winning number 19
+
+let winningNumber = 19;
+// "12"
+// let userGuess = +prompt("Enetr a no.");
+// console.log(typeof userGuess, userGuess);
+
+//switch
+
+let day = 4;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wedneday");
+    break;
+  case 4:
+    console.log("Thrush");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Sat");
+    break;
+  case 7:
+    console.log("Sun");
+    break;
+  default:
+    console.log("invalid");
+    break;
+}
+
+//loop
+//while
+
+let i = 0;
+while (i < 11) {
+  console.log(i);
+  i++;
+}
+
+for (i = 0; i < 12; i += 2) {
+  console.log(i);
+}
---
+++ file2.js
@@ -0,0 +1,29 @@
+//loop
+//let temp basic
+//var global
+
+//exit controller
+var i = 0;
+do {
+  console.log(`i am the ${i} \n`);
+  i++;
+} while (i < 9);
+
+var i = 0;
+
+//entry controller
+while (i < 9) {
+  console.log(`i am the ${i}`);
+  i++;
+}
+
+//array
+//intro to arrays
+// reference type
+// how to create arrays
+// ordered collection of items
+let numbers = ["apple", "mango", 5];
+console.log(numbers);
+//just like python
+//array method : .push --> append ; .pop --> remove last elemtn
+// .shift --> remove 0th element ; unshift ---> add elements to 1sts
